0569664b80b6789c58768bbbef2d7a65f752d7b8
Duxz <semada15@gmail.com>
2021-08-28 16:30:07 +0700

    fix

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "./Dashboard"
+
+const Home = () => {
+  return (
+    <>
+      <div className="">
+        <Dashboard/>
+      </div>
+        </>
+  )
+}
+
+export default Home
